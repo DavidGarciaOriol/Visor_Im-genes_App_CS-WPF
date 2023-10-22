5b3d400b586028e0ef1d1fad5737ce21d3009778
davidgarciaoriol <davidgarciaoriol@gmail.com>
2023-10-22 22:11:25 +0200

    Terminada app visualización imágenes con DropArea

--- VisualizadorImágenesWPF/MainWindow.xaml.cs
+++ VisualizadorImágenesWPF/MainWindow.xaml.cs
@@ -1,46 +1,80 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
 
-namespace VisualizadorImágenesWPF
+/**
+ * El método del drop-área ha sido confeccionado con la ayuda del modelo de lenguaje GPT 3.5.
+ * Los comentarios correspondientes a esa parte del código son confeccionados por mí para demostrar el entendimiento
+ * del fragmento de código y su integración, así como para facilitar su comprensión para reutilización de a futuro.
+ */
+
+namespace ImageViewer
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        private string rutaArchivo = "";
-        private string[] rutasRecientes = Array.Empty<string>();
+        // Lista de imágenes recientes.
+        private List<string> imagePaths = new List<string>();
 
         public MainWindow()
         {
             InitializeComponent();
+            AllowDrop = true;
+            Drop += ImageDrop; // Se llama al método al soltar un archivo en la ventana. Se puede hacer así o poner el enlace al método del evento en el componente directamente en el XAML. Lo destaco porque me he fijado en ello a raíz de este proyecto.
         }
 
-        private void abrir_Click(object sender, RoutedEventArgs e)
+        // Se añade la imagen dropeada en el drop-área a la lista de recientes y se muestra en el marco.
+        private void ImageDrop(object sender, DragEventArgs e)
         {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                foreach (string file in files)
+                {
+                    if (IsImageFile(file))
+                    {
+                        imagePaths.Add(file); // Se añade a la lista de rutas de recientes.
+                        AddRecentImageMenuItem(file); // Añade un nuevo componente a la lista de recientes que contendrá la ruta de la nueva imagen reciente.
+                        DisplayImage(file); // La imagen se muestra en el visualizador.
+                    }
+                }
+            }
         }
 
-        private void abrirReciente_Click(object sender, RoutedEventArgs e)
+        // Muestra la imagen recibida por parámetros en forma de path en el visualizador.
+        private void DisplayImage(string imagePath)
         {
-            MenuItem menuItemRuta = new MenuItem();
+            BitmapImage image = new BitmapImage(new Uri(imagePath));
+            displayedImage.Source = image;
+        }
 
-            rutasRecientes.Append("Patata");
+        // Se comprueba que el archivo dropeado sea un archivo de imagen.
+        private bool IsImageFile(string filePath)
+        {
+            string extension = System.IO.Path.GetExtension(filePath).ToLower();
+            return extension == ".jpg" || extension == ".jpeg" || extension == ".png" || extension == ".gif" || extension == ".bmp";
+        }
 
-            menuItemRuta.Header = rutasRecientes[0];
-            menuRecientes.Items.Add(menuItemRuta);
+        // Método que abre la imagen reciente al clickar en el componente con la ruta de la imagen añadida anteriormente.
+        private void OpenImage_Click(object sender, RoutedEventArgs e)
+        {
+            string imagePath = ((MenuItem)sender).Tag.ToString();
+            DisplayImage(imagePath);
+        }
+
+        // Método que confecciona dinámicamente y añade componentes de path recientes al menú de recientes.
+        private void AddRecentImageMenuItem(string imagePath)
+        {
+            MenuItem recentImageMenuItem = new MenuItem();
+            recentImageMenuItem.Header = System.IO.Path.GetFileName(imagePath);
+            recentImageMenuItem.Tag = imagePath;
+            recentImageMenuItem.Click += OpenImage_Click; // Se enlaza el evento al componente de forma dinámica por código.
+
+            // Agregar el elemento al submenú "Recientes"
+            RecientesMenu.Items.Insert(0, recentImageMenuItem);
         }
     }
 }
