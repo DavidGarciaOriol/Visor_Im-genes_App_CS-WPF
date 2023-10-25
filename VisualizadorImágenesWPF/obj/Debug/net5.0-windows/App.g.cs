2f726e7205a9c9a82f8afc0014285bad1eeaf9b7
ciclot <ciclot@A2.11_PC20>
2023-10-25 18:26:39 +0200

    Añadido Acerca de

--- VisualizadorImágenesWPF/obj/Debug/net5.0-windows/App.g.cs
+++ VisualizadorImágenesWPF/obj/Debug/net5.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace VisualizadorImágenesWPF {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.17.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace VisualizadorImágenesWPF {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.17.0")]
         public static void Main() {
             VisualizadorImágenesWPF.App app = new VisualizadorImágenesWPF.App();
             app.InitializeComponent();
